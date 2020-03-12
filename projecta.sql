a8fa327573e76d1730a920e386351a9c09c0cae2
Nutchanon Phongam <oakbaza@gmail.com>
2020-03-12 09:23:37 +0700

    add database

---
+++ projecta.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2020 at 03:22 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projecta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL,
+  `chat_sender` int(11) NOT NULL,
+  `chat_reciever` int(11) NOT NULL,
+  `chat_message` varchar(255) NOT NULL,
+  `chat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_name_tmp` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_fname` varchar(255) NOT NULL,
+  `user_lname` varchar(255) NOT NULL,
+  `user_birthdate` date NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_address` varchar(255) NOT NULL,
+  `user_picture` varchar(255) NOT NULL,
+  `user_picture_old` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `user_name_tmp`, `user_password`, `user_fname`, `user_lname`, `user_birthdate`, `user_email`, `user_address`, `user_picture`, `user_picture_old`) VALUES
+(5, 'username', 'username', 'username', 'user', 'user', '2020-03-10', 'oakbaza@gmail.com', 'asdasd', '1583593697.png', 'cats.png'),
+(6, 'sirinat', 'sirinatttt', 'sirinat', 'sirinattt', 'sirinataaaa', '2020-03-17', 'example@xxxx.comasd', 'asdas asd asd asd asd aszzzxc', '1583806766.jpeg', 'profile.jpg'),
+(7, 'natcha', 'natcha', 'natcha', 'natcha', 'buatim', '1998-02-02', 'natcha@gmail.com', 'bangkok', '1583957821.jpeg', 'cat.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chat_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
